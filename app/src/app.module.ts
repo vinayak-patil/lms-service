35e3f7377efa975463a33afdf21605c041f477ed
Vinayak Patil <vinayak.patil@tekditechnologies.com>
2025-05-20 22:19:30 +0530

    Remove testapis

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,6 +1,5 @@
-import { Module, NestModule } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './common/database.module';
 import { AppService } from './app.service';
