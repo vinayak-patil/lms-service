d410ccc67e8bc210d3ac7f478c25469c73f702ca
Vinayak Patil <vinayak.patil@tekditechnologies.com>
2025-05-20 16:04:21 +0530

    Update docker

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-
+import { DatabaseModule } from './common/database.module';
 
 @Module({
   imports: [
@@ -13,25 +13,7 @@ import { AppController } from './app.controller';
     }),
     
     // Database
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('PGHOST'),
-        port: configService.get('PGPORT'),
-        username: configService.get('PGUSER'),
-        password: configService.get('PGPASSWORD'),
-        database: configService.get('PGDATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        logging: configService.get('NODE_ENV') === 'development',
-        autoLoadEntities: false,
-        ssl: configService.get('PGSSLMODE') === 'no-verify' ? false : {
-          rejectUnauthorized: false
-        },
-      }),
-    }),
+    DatabaseModule,
     
   ],
   controllers: [AppController],
--- app/src/common/database.module.ts
+++ app/src/common/database.module.ts
@@ -6,13 +6,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get("POSTGRES_HOST"),
-        port: configService.get("POSTGRES_PORT"),
-        database: configService.get("POSTGRES_DATABASE"),
-        username: configService.get("POSTGRES_USERNAME"),
-        password: configService.get("POSTGRES_PASSWORD"),
-        autoLoadEntities: true,
+        type: 'postgres',
+        host: configService.get('PGHOST'),
+        port: configService.get('PGPORT'),
+        username: configService.get('PGUSER'),
+        password: configService.get('PGPASSWORD'),
+        database: configService.get('PGDATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+        autoLoadEntities: false,
       }),
       inject: [ConfigService],
     }),
